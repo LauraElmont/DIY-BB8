6596f9704e4ede5e4e87d919752f0e38cd9d26fb
MIRABEAU\lelmont <lelmont@mirabeau.nl>
2017-03-27 11:45:33 +0200

    Add basic solution to webapp folder

---
+++ src/WebApp/DIY.BB8/DIY.BB8.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DIY.BB8.Web.Startup))]
+namespace DIY.BB8.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
